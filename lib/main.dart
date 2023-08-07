14d3a54d781c0ad446e8d0cc8c03aa486a84eb7d
Yuri_Grj <grj0824@gmail.com>
2023-08-08 04:18:04 +0530

    rounded_button

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:custom_widget/rounded_btn.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Yuri Grj',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Yuri & Moon Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text("Yuri&Moon"),
+      ),
+      body: Container(
+        width: 100,
+        child: RoundedButton(
+          btnName: 'Login',
+          icon: Icon(Icons.lock),
+          callBack: () {
+            print('Logged in');
+          },
+          textStyle: mTextStyle16(),
+        ),
+      ),
+    );
+  }
+}
